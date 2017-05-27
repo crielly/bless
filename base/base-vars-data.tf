d7a5d0e75b5a04ce94ea6bfa4a09ec4b3b83190b
Connor Rielly <crielly@outlook.com>
2017-05-27 00:09:08 -0400

    add global iam data source

--- base/base-vars-data.tf
+++ base/base-vars-data.tf
@@ -12,3 +12,12 @@ data "aws_region" "current" {
 }
 
 data "aws_caller_identity" "current" {}
+
+data "terraform_remote_state" "iam-global" {
+    backend    = "s3"
+    config {
+        bucket = "${var.namespace}-tfstate.${data.aws_region.current.name}"
+        key    = "iam-global/terraform.tfstate"
+        region = "${data.aws_region.current.name}"
+    }
+}
